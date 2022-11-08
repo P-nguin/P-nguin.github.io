aa953380cfd3e45a702f6363c54e0523ee40675e
Penquion <zhaodaniel2005@gmail.com>
2022-11-07 22:27:56 -0500

    Planning Layout, changes to design

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const landingPageVideo = document.getElementById("videoLandingPage");
-
-landingPageVideo.style.width = innerWidth;
-landingPageVideo.addEventListener('contextmenu', (event) => {
-    console.log(event)
-    event.preventDefault();
-}, false);
